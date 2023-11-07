f5dafbd911277caf7d51c0f808d9a4899c6f2c57
selb05 <kashey-94@mail.ru>
2023-11-07 12:02:51 +0300

    Update calculator.go

--- calculator.go
+++ calculator.go
@@ -1,67 +1,154 @@
 package main
 
 import (
+	"bufio" //для чтения ввода пользователя
 	"fmt"
+	"log" //для ведения логов ошибки
+	"os" //для работы с операционной системой
+	"sort" //для сортировки ключей в карте римских чисел
 	"strconv"
+	"strings"
 )
 
 func main() {
-	for {
-		fmt.Println("Введите операцию (+, -, *, /) или 'exit' для выхода:")
-		operator := readOperator()
-		if operator == "exit" {
-			break
-		}
 
-		fmt.Println("Введите первое целое число:")
-		num1 := readInteger()
-		fmt.Println("Введите второе целое число:")
-		num2 := readInteger()
+	reader := bufio.NewReader(os.Stdin)
 
-		result, err := calculate(num1, operator, num2)
-		if err != nil {
-			fmt.Println("Ошибка при выполнении операции:", err)
-		} else {
-			fmt.Printf("Результат: %v\n", result)
-		}
-	}
-}
-
-func readOperator() string {
-    var operator string
-    fmt.Scanln(&operator)
-    if operator != "+" && operator != "-" && operator != "*" && operator != "/" {
-        fmt.Println("Неверный оператор. Допустимые операторы: +, -, *, /")
-        return readOperator()
-    }
-    return operator
-}
-
-func readInteger() int {
-	var input string
-	fmt.Scanln(&input)
-	num, err := strconv.Atoi(input)
+	input, err := reader.ReadString('\n')
 	if err != nil {
-		fmt.Println("Неверный формат числа. Введите целое число")
-		return readInteger()
+		log.Printf("Ошибка в прочтении ввода: %s %s", input, err)
 	}
-	return num
+
+	if strings.ContainsAny(input, "+-*/") == false {
+		fmt.Println("Вывод ошибки, так как строка не является математической операцией.")
+		return
+	}
+
+	numbers, operation := splitIntoTwoNumbersAndOperation(input) //которая разбивает введенную строку на два операнда и операцию
+	if operation == "" {
+		fmt.Println("Вывод ошибки, так как строка не является математической операцией.")
+		return
+	}
+	if len(numbers) > 2 {
+		fmt.Println("Вывод ошибки, так как формат математической операции не удовлетворяет заданию — два операнда и один оператор (+, -, /, *).")
+		return
+	}
+
+	a, isRoman1 := parseNum(numbers[0])
+	b, isRoman2 := parseNum(numbers[1])
+
+	if isRoman1 != isRoman2 {
+		fmt.Println("Вывод ошибки, так как используются одновременно разные системы счисления.")
+		return
+	}
+
+	var result int
+
+	switch operation {
+	case "+":
+		result = a + b
+	case "-":
+		result = a - b
+	case "*":
+		result = a * b
+	case "/":
+		result = a / b
+	}
+
+	// Выводим результат
+	if isRoman1 {
+		if result <= 0 {
+			fmt.Println("Вывод ошибки, так как в римской системе нет отрицательных чисел.")
+			return
+		}
+		fmt.Println(toRoman(result))
+		return
+	}
+	fmt.Println(result)
+
 }
 
-func calculate(x int, operator string, y int) (int, error) {
-	switch operator {
-	case "+":
-		return x + y, nil
-	case "-":
-		return x - y, nil
-	case "*":
-		return x * y, nil
-	case "/":
-		if y == 0 {
-			return 0, fmt.Errorf("Деление на ноль")
-		}
-		return x / y, nil
-	default:
-		return 0, fmt.Errorf("Неизвестный оператор: %s", operator)
+func parseNum(numStr string) (int, bool) { //пытается преобразовать строковое представление операнда в целое число. Если это не удается, программа выводит сообщение об ошибке и завершает выполнение
+	romanMap := map[string]int{
+		"I":    1,
+		"II":   2,
+		"III":  3,
+		"IV":   4,
+		"V":    5,
+		"VI":   6,
+		"VII":  7,
+		"VIII": 8,
+		"IX":   9,
+		"X":    10,
 	}
+
+	if num, ok := romanMap[numStr]; ok {
+		return num, true
+
+	}
+	num, err := strconv.Atoi(numStr)
+	if err != nil {
+		log.Printf("Не смог конвертировать стринг %s в инт, err: %s", numStr, err)
+		return 1, false
+	}
+
+	return num, false
+
+}
+
+func toRoman(num int) string {//выполняет конвертацию арабских чисел в римские числа. Она использует карту соответствий чисел и римских символов для выполнения преобразования
+	romanMap := map[int]string{
+		1:   "I",
+		4:   "IV",
+		5:   "V",
+		9:   "IX",
+		10:  "X",
+		40:  "XL",
+		50:  "L",
+		90:  "XC",
+		100: "C",
+	}
+
+	var keys []int
+	for k := range romanMap {
+		keys = append(keys, k)
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(keys)))
+
+	result := ""
+	for _, value := range keys {
+		symbol := romanMap[value]
+		for num >= value {
+			result += symbol
+			num -= value
+		}
+	}
+	return result
+}
+
+func splitIntoTwoNumbersAndOperation(input string) ([]string, string) {
+
+	switch {
+	case strings.Contains(input, "+"):
+		input = strings.Replace(input, " ", "", -1)
+		input = strings.Replace(input, "\r\n", "", -1)
+		nums := strings.Split(input, "+")
+		return nums, "+"
+	case strings.Contains(input, "-"):
+		input = strings.Replace(input, " ", "", -1)
+		input = strings.Replace(input, "\r\n", "", -1)
+		nums := strings.Split(input, "-")
+		return nums, "-"
+	case strings.Contains(input, "/"):
+		input = strings.Replace(input, " ", "", -1)
+		input = strings.Replace(input, "\r\n", "", -1)
+		nums := strings.Split(input, "/")
+		return nums, "/"
+	case strings.Contains(input, "*"):
+		input = strings.Replace(input, " ", "", -1)
+		input = strings.Replace(input, "\r\n", "", -1)
+		nums := strings.Split(input, "*")
+		return nums, "*"
+	}
+	return nil, ""
 }
